152ce9a1325c92b48ad6ea38665933569474116d
Kevin <vargaskevv@gmail.com>
2023-01-29 14:27:34 -0400

    Update premium card UI

--- src/lib/sections/Pricing.svelte
+++ src/lib/sections/Pricing.svelte
@@ -9,12 +9,12 @@
 	heading="Disfruta de nuestros planes anuales"
 	description=""
 />
-<section class="bg-neutral-50 dark:bg-dark">
+<section class="bg-neutral-50 dark:bg-dark pb-12">
 	<div
 		class="mx-auto max-w-2xl w-full px-4 grid gap-x-8 gap-y-4 grid-cols-1 sm:grid-cols-2 dark:text-white"
 	>
 		<div
-			class="flex flex-col items-center px-6 py-8 shadow-md dark:shadow-neutral-600 rounded-lg bg-white dark:bg-neutral-800"
+			class="flex flex-col items-center px-6 py-8 shadow-xl dark:shadow-neutral-600 rounded-lg bg-white dark:bg-neutral-800"
 		>
 			<div class="text-neutral-500 mb-2">
 				<Icon class="w-12 h-12" icon="material-symbols:app-shortcut" />
@@ -35,24 +35,22 @@
 			</div>
 		</div>
 		<div
-			class="flex flex-col items-center px-6 py-8 shadow-md dark:shadow-neutral-600 rounded-lg bg-white dark:bg-neutral-800"
+			class="flex flex-col items-center px-6 py-8 shadow-xl shadow-primarylight dark:shadow-primary rounded-lg bg-primary dark:bg-primarydark text-white"
 		>
-			<div class="text-yellow-400 dark:text-yellow-300 mb-2">
+			<div class="text-yellow-300 mb-2">
 				<Icon class="w-12 h-12" icon="ic:round-star" />
 			</div>
-			<p class="text-xl font-semibold text-neutral-700 dark:text-neutral-100 tracking-wider">
-				PREMIUM
-			</p>
-			<p class="text-4xl font-bold my-4 text-neutral-400 dark:text-neutral-500 line-through">
+			<p class="text-xl font-semibold text-neutral-100 tracking-wider">PREMIUM</p>
+			<p class="text-4xl font-bold my-4 text-neutral-300 dark:text-neutral-400 line-through">
 				150 Bs.
 			</p>
 			<p
-				class="text-4xl font-bold text-green-700 dark:text-green-500 leading-3 -translate-x-3 animate-bounce ease-out"
+				class="text-4xl font-bold text-green-400 dark:text-green-500 leading-3 -translate-x-3 animate-bounce ease-out"
 			>
 				100 Bs.
 			</p>
 			<p class="text-sm font-bold my-4 italic translate-x-12 -translate-y-2 -rotate-12">
-				¡Sólo por el 2023!
+				¡Sólo por 2023!
 			</p>
 			<div class="self-start">
 				<ul class="list-disc ml-4">
