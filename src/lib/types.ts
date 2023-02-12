540dcc216763b38c5cd5091fbaa93d4956d8e141
Kevin <vargaskevv@gmail.com>
2023-02-12 00:09:14 -0400

    Add class details response type;

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -11,3 +11,104 @@ export type ServerData = {
 		activities: number;
 	};
 };
+
+export enum Attendance {
+	Presente = "Presente",
+	Falta = "Falta",
+	Licencia = "Licencia",
+	Atraso = "Atraso",
+}
+
+export type ClassDetails = {
+	class_data: {
+		id: string;
+		parallel: string;
+		municipio: string;
+		provincia: string;
+		departamento: string;
+		school: {
+			id: string;
+			name: string;
+			lat: string;
+			lon: string;
+		};
+		teacher: {
+			id: string;
+			name: string;
+			last_name: string;
+			email: string;
+			is_admin: boolean;
+		};
+		subject: {
+			id: string;
+			name: string;
+		};
+		grade: {
+			id: string;
+			name: string;
+		};
+		year: {
+			id: string;
+			value: number;
+		};
+	};
+	students: {
+		id: string;
+		name: string;
+		last_name: string;
+		ci: string;
+		year_score: number;
+		scores_map: {
+			[activity_id: string]: {
+				id: string;
+				points: number;
+			};
+		};
+		scores: {
+			[class_period_id: string]: {
+				score: number;
+				area_scores: {
+					[area_id: string]: number;
+				};
+			};
+		};
+		attendances_map: {
+			[att_day_id: string]: {
+				id: string;
+				value: Attendance;
+			};
+		};
+		year_total_attendances: {
+			[key in Attendance]: number;
+		};
+		period_total_attendances: {
+			[class_period_id: string]: {
+				[key in Attendance]: number;
+			};
+		};
+	}[];
+	class_periods: {
+		id: string;
+		start: number;
+		end: number;
+		finished: boolean;
+		period: {
+			id: string;
+			name: string;
+		};
+		areas: {
+			id: string;
+			name: string;
+			points: number;
+			activities: {
+				id: string;
+				name: string;
+				date: number;
+			}[];
+		}[];
+		attendance_days: {
+			id: string;
+			day: number;
+		}[];
+	}[];
+};
