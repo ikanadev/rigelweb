c3c8b4d840d19f3c59d76a25e6ccfca7b2f31c2e
Kevin <vargaskevv@gmail.com>
2023-02-14 15:08:33 -0400

    Class scores add Detailed, by area and by period filters.

--- src/routes/(class)/class/[id]/+page.svelte
+++ src/routes/(class)/class/[id]/+page.svelte
@@ -143,48 +143,69 @@
 			<table>
 				<thead class="bg-slate-100">
 					<tr>
-						<th rowspan="3" class="sticky left-0 bg-slate-100 whitespace-nowrap"
-							>Nombre(s) y apellido(s)</th>
+						<th
+							rowspan={scoresView === ScoresView.Detailed
+								? 3
+								: scoresView === ScoresView.Area
+								? 2
+								: 1}
+							class="sticky left-0 bg-slate-100 whitespace-nowrap">
+							Nombre(s) y apellido(s)
+						</th>
 						{#each class_periods as cp}
 							<th
 								class="bg-slate-500 text-xl font-bold text-white border-r-8 border-white"
-								colspan={cp.areas.length +
-									cp.areas.reduce((sum, area) => sum + area.activities.length, 0) +
-									1}>
+								colspan={cp.areas.reduce((sum, area) => {
+									if (scoresView === ScoresView.Period) return sum;
+									if (scoresView === ScoresView.Area) return sum + 1;
+									return sum + area.activities.length + 1;
+								}, 0) + 1}>
 								{cp.period.name}
 							</th>
 						{/each}
-						<th rowspan="3" colspan="2" class="bg-slate-800 text-white font-bold text-xl"
-							>NOTA FINAL</th>
+						<th
+							rowspan={scoresView === ScoresView.Detailed
+								? 3
+								: scoresView === ScoresView.Area
+								? 2
+								: 1}
+							colspan="2"
+							class="bg-slate-800 text-white font-bold text-xl">
+							NOTA FINAL
+						</th>
 					</tr>
-					<tr>
-						{#each class_periods as cp}
-							{#each cp.areas as area}
-								<th
-									colspan={area.activities.length + 1}
-									class="bg-slate-300 border-r-2 border-white font-semibold whitespace-nowrap">
-									{area.name} ({area.points})
-								</th>
-							{/each}
-							<th
-								rowspan="2"
-								class="bg-slate-500 text-xl font-bold text-white border-r-8 border-white">
-								Nota
-							</th>
-						{/each}
-					</tr>
-					<tr>
-						{#each class_periods as cp}
-							{#each cp.areas as area}
-								{#each area.activities as act}
-									<th class="whitespace-nowrap text-sm border-r border-white">
-										{act.name.length > 15 ? `${act.name.substring(0, 12)}.` : act.name}
+					{#if scoresView === ScoresView.Detailed || scoresView === ScoresView.Area}
+						<tr>
+							{#each class_periods as cp}
+								{#each cp.areas as area}
+									<th
+										colspan={scoresView === ScoresView.Detailed ? area.activities.length + 1 : 1}
+										class="bg-slate-300 border-r-2 border-white font-semibold whitespace-nowrap">
+										{area.name} ({area.points})
 									</th>
 								{/each}
-								<th class="bg-slate-300 border-r-2 border-white font-semibold">Prom.</th>
+								<th
+									rowspan={scoresView === ScoresView.Detailed ? 2 : 1}
+									class="bg-slate-500 text-xl font-bold text-white border-r-8 border-white">
+									Nota
+								</th>
 							{/each}
-						{/each}
-					</tr>
+						</tr>
+					{/if}
+					{#if scoresView === ScoresView.Detailed}
+						<tr>
+							{#each class_periods as cp}
+								{#each cp.areas as area}
+									{#each area.activities as act}
+										<th class="whitespace-nowrap text-sm border-r border-white">
+											{act.name.length > 15 ? `${act.name.substring(0, 12)}.` : act.name}
+										</th>
+									{/each}
+									<th class="bg-slate-300 border-r-2 border-white font-semibold">Prom.</th>
+								{/each}
+							{/each}
+						</tr>
+					{/if}
 				</thead>
 				<tbody class="bg-slate-100">
 					{#each filteredStudents as student}
@@ -198,17 +219,22 @@
 								{student.name}
 							</td>
 							{#each class_periods as cp}
-								{#each cp.areas as area}
-									{#each area.activities as act}
-										<td class="border-r border-white">
-											{student.scores_map[act.id]?.points ?? '-'}
+								{#if scoresView === ScoresView.Area || scoresView === ScoresView.Detailed}
+									{#each cp.areas as area}
+										{#if scoresView === ScoresView.Detailed}
+											<!-- Nota actividad -->
+											{#each area.activities as act}
+												<td class="border-r border-white">
+													{student.scores_map[act.id]?.points ?? '-'}
+												</td>
+											{/each}
+										{/if}
+										<!-- Nota area -->
+										<td class="bg-slate-300 border-r-2 border-white font-semibold">
+											{student.scores[cp.id].area_scores[area.id]}
 										</td>
 									{/each}
-									<!-- Nota area -->
-									<td class="bg-slate-300 border-r-2 border-white font-semibold">
-										{student.scores[cp.id].area_scores[area.id]}
-									</td>
-								{/each}
+								{/if}
 								<!-- Nota periodo (nota trimestral)-->
 								<td class="border-r-8 border-white bg-slate-500 text-white text-lg font-bold">
 									{student.scores[cp.id].score}
