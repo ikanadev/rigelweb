2b91ff7fd2d23cdec2e0d4d03455b2eb470e6040
Kevin <vargaskevv@gmail.com>
2023-02-14 20:34:51 -0400

    Show 404 response for unknow classs

--- src/routes/(class)/class/[id]/+page.ts
+++ src/routes/(class)/class/[id]/+page.ts
@@ -1,3 +1,4 @@
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 import type { ClassDetails } from "$lib/types";
 
@@ -5,6 +6,9 @@ export const load = (async ({ params, fetch }) => {
 	const url = `${import.meta.env.VITE_API_URL}class/${params.id}`;
 	const resp = await fetch(url);
 	if (!resp.ok) {
+		if (resp.status === 404) {
+			throw error(404, "Clase no encontrada");
+		}
 		throw new Error("Error fetching class details");
 	}
 	const data: ClassDetails = await resp.json();
