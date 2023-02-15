b1a29bdff32ade51c391880c47abbe244997f305
Kevin <vargaskevv@gmail.com>
2023-02-14 20:14:01 -0400

    Class attendance table and filters

---
+++ src/lib/comps/Attendance.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Attendance } from '$lib/types';
+
+	export let attendance: Attendance;
+
+	const attendanceColors: { [key in Attendance]: string } = {
+		[Attendance.Presente]: 'rgb(21, 128, 61)',
+		[Attendance.Falta]: 'rgb(239, 68, 68)',
+		[Attendance.Atraso]: 'rgb(251, 146, 60)',
+		[Attendance.Licencia]: 'rgb(37, 99, 235)'
+	};
+</script>
+
+<p
+	class="rounded border text-sm font-mono px-0.5 font-semibold leading-4 bg-white"
+	style="color:{attendanceColors[attendance]}; border-color: {attendanceColors[attendance]};">
+	{attendance.substring(0, 1)}
+</p>
--- src/routes/(class)/class/[id]/+page.svelte
+++ src/routes/(class)/class/[id]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { APP_NAME } from '$lib/constants';
-	import type { PageData } from './$types';
 	import { page } from '$app/stores';
+	import dayjs from 'dayjs';
+	import type { PageData } from './$types';
+	import Attendance from '$lib/comps/Attendance.svelte';
+	import { Attendance as Att } from '$lib/types';
+	import 'dayjs/locale/es-mx';
+	dayjs.locale('es-mx');
 
 	enum ScoresView {
 		Detailed = 'Detailed',
@@ -12,10 +17,9 @@
 	export let data: PageData;
 	const { class_data, class_periods, students } = data;
 	let scoresView: ScoresView = ScoresView.Detailed;
+	let showAttDays = true;
 	let showScores = true;
 	let studentFilter = '';
-
-	console.log(data);
 
 	$: filteredStudents =
 		studentFilter.trim() === ''
@@ -135,11 +139,19 @@
 			</select>
 		</div>
 	{:else}
-		<div>Attendances controls</div>
+		<div class="flex justify-end items-center mb-2">
+			<p>Buscar:</p>
+			<input
+				bind:value={studentFilter}
+				placeholder="Nombre o apellido"
+				class="border mr-4 ml-1 px-2 py-0.5 rounded bg-slate-100" />
+			<label for="showAttDays" class="mr-1"> Mostrar d&iacute;as </label>
+			<input id="showAttDays" bind:checked={showAttDays} type="checkbox" />
+		</div>
 	{/if}
 
 	{#if showScores}
-		<div id="table_container" class="relative overflow-x-auto pb-4">
+		<div class="table_container relative overflow-x-auto pb-4">
 			<table>
 				<thead class="bg-neutral-100">
 					<tr>
@@ -253,7 +265,127 @@
 			</table>
 		</div>
 	{:else}
-		<div>Attendances</div>
+		<div class="flex justify-end items-center font-medium gap-1 mb-2">
+			<p>{Att.Presente}</p>
+			<Attendance attendance={Att.Presente} />
+			<p class="ml-2">{Att.Atraso}</p>
+			<Attendance attendance={Att.Atraso} />
+			<p class="ml-2">{Att.Falta}</p>
+			<Attendance attendance={Att.Falta} />
+			<p class="ml-2">{Att.Licencia}</p>
+			<Attendance attendance={Att.Licencia} />
+		</div>
+		<div class="table_container relative overflow-x-auto pb-4">
+			<table>
+				<thead>
+					<tr>
+						<th rowspan="2" class="whitespace-nowrap sticky left-0 bg-neutral-100">
+							Apellido(s) y nombre(s)
+						</th>
+						{#each class_periods as cp}
+							<th
+								colspan={4 + (showAttDays ? cp.attendance_days.length : 0)}
+								class="whitespace-nowrap bg-slate-200 border-r-8 border-white">
+								{cp.period.name}
+							</th>
+						{/each}
+						<th colspan="4" class="bg-slate-500 text-white">Total</th>
+					</tr>
+					<tr>
+						{#each class_periods as cp}
+							{#if showAttDays}
+								{#each cp.attendance_days as attDay}
+									<th class="font-medium text-sm leading-4 bg-neutral-100 border-r border-white">
+										{dayjs(attDay.day).format('ddd\nDD/MM')}
+									</th>
+								{/each}
+							{/if}
+							<th class="bg-slate-200">
+								<div class="flex justify-center px-1">
+									<Attendance attendance={Att.Presente} />
+								</div>
+							</th>
+							<th class="bg-slate-200">
+								<div class="flex justify-center px-1">
+									<Attendance attendance={Att.Atraso} />
+								</div>
+							</th>
+							<th class="bg-slate-200">
+								<div class="flex justify-center px-1">
+									<Attendance attendance={Att.Falta} />
+								</div>
+							</th>
+							<th class="bg-slate-200 border-r-8 border-white">
+								<div class="flex justify-center px-1">
+									<Attendance attendance={Att.Licencia} />
+								</div>
+							</th>
+						{/each}
+						<th class="bg-slate-500">
+							<div class="flex justify-center px-1.5"><Attendance attendance={Att.Presente} /></div>
+						</th>
+						<th class="bg-slate-500">
+							<div class="flex justify-center px-1.5"><Attendance attendance={Att.Atraso} /></div>
+						</th>
+						<th class="bg-slate-500">
+							<div class="flex justify-center px-1.5"><Attendance attendance={Att.Falta} /></div>
+						</th>
+						<th class="bg-slate-500">
+							<div class="flex justify-center px-1.5"><Attendance attendance={Att.Licencia} /></div>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each filteredStudents as student}
+						<tr class="bg-neutral-100 border-t border-white">
+							<td class="whitespace-nowrap pt-1 sticky left-0 bg-neutral-100">
+								{student.last_name}
+								{student.name}
+							</td>
+							{#each class_periods as cp}
+								{#if showAttDays}
+									{#each cp.attendance_days as attDay}
+										<td class="border-r border-white">
+											<div class="flex justify-center">
+												{#if student.attendances_map[attDay.id] !== undefined}
+													<Attendance attendance={student.attendances_map[attDay.id].value} />
+												{:else}
+													-
+												{/if}
+											</div>
+										</td>
+									{/each}
+								{/if}
+								<td class="bg-slate-200 text-center font-medium">
+									{student.period_total_attendances[cp.id].Presente}
+								</td>
+								<td class="bg-slate-200 text-center font-medium">
+									{student.period_total_attendances[cp.id].Atraso}
+								</td>
+								<td class="bg-slate-200 text-center font-medium">
+									{student.period_total_attendances[cp.id].Falta}
+								</td>
+								<td class="bg-slate-200 text-center font-medium border-r-8 border-white">
+									{student.period_total_attendances[cp.id].Licencia}
+								</td>
+							{/each}
+							<td class="bg-slate-500 text-white font-semibold text-center text-lg">
+								{student.year_total_attendances.Presente}
+							</td>
+							<td class="bg-slate-500 text-white font-semibold text-center text-lg">
+								{student.year_total_attendances.Atraso}
+							</td>
+							<td class="bg-slate-500 text-white font-semibold text-center text-lg">
+								{student.year_total_attendances.Falta}
+							</td>
+							<td class="bg-slate-500 text-white font-semibold text-center text-lg">
+								{student.year_total_attendances.Licencia}
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
 	{/if}
 </main>
 
@@ -267,20 +399,20 @@
 		font-family: 'Assistant', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
 			Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
-	#table_container::-webkit-scrollbar {
+	.table_container::-webkit-scrollbar {
 		width: 8px;
 		height: 8px;
 	}
-	#table_container::-webkit-scrollbar-track {
+	.table_container::-webkit-scrollbar-track {
 		border-radius: 4px;
 		background-color: #e7e7e7;
 		border: 1px solid #cacaca;
 	}
-	#table_container::-webkit-scrollbar-thumb {
+	.table_container::-webkit-scrollbar-thumb {
 		border-radius: 4px;
 		background-color: #999;
 	}
-	#table_container::-webkit-scrollbar-thumb:hover {
+	.table_container::-webkit-scrollbar-thumb:hover {
 		background-color: #777;
 	}
 </style>
