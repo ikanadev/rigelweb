7df7aa1f2bc535be80e93c1333fbdf9090a4191f
Kevin <vargaskevv@gmail.com>
2023-03-16 10:00:38 -0400

    Rework embedded video for performance reasons

--- src/lib/sections/Videos.svelte
+++ src/lib/sections/Videos.svelte
@@ -1,47 +1,56 @@
 <script lang="ts">
 	import SectionTitle from '$lib/comps/SectionTitle.svelte';
+	import { onMount } from 'svelte';
+
+	let ytCont: HTMLDivElement;
+	const videos = [
+		{ label: 'Instalación', url: 'https://youtu.be/3ebdT56LOT0' },
+		{ label: 'Clases o materias', url: 'https://youtu.be/EoCDqeZXrIo' },
+		{ label: 'Periodos (Trimestres)', url: 'https://youtu.be/4QgcE2l8dtg' },
+		{ label: 'Administración de estudiantes', url: 'https://youtu.be/oUQ0prTDRkQ' },
+		{ label: 'Asistencias', url: 'https://youtu.be/aHvqUywurGo' },
+		{ label: 'Actividates (tareas, exámenes, etc.)', url: 'https://youtu.be/YqL-oM8f53k' }
+	];
+
+	onMount(() => {
+		if (!ytCont) return;
+		const thumbSource = `https://img.youtube.com/vi/${ytCont.dataset.embed}/sddefault.jpg`;
+		const thumbImg = new Image();
+		thumbImg.src = thumbSource;
+		thumbImg.addEventListener('load', () => {
+			ytCont.appendChild(thumbImg);
+		});
+		ytCont.addEventListener('click', () => {
+			const iframe = document.createElement('iframe');
+			iframe.setAttribute('frameborder', '0');
+			iframe.setAttribute('allowfullscreen', '');
+			iframe.setAttribute(
+				'src',
+				`https://www.youtube.com/embed/${ytCont.dataset.embed}?rel=0&showinfo=0&autoplay=1`
+			);
+			ytCont.innerHTML = '';
+			ytCont.appendChild(iframe);
+		});
+	});
 </script>
 
 <SectionTitle id="videos" title="Videos" heading="Échale un vistazo a Auleca" description="" />
 <div class="bg-neutral-50 dark:bg-dark text-neutral-800 dark:text-white pb-20">
 	<section class="container mx-auto px-2">
 		<div class="flex flex-col lg:flex-row items-center lg:items-start gap-8">
-			<div class="px-0 lg:px-10">
-				<iframe
-					class="max-w-full"
-					width="560"
-					height="315"
-					src="https://www.youtube.com/embed/jIlbrmwX49A"
-					title="YouTube video player"
-					frameborder="0"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-					allowfullscreen />
+			<div class="px-0 lg:px-10 flex-1">
+				<div class="youtube" data-embed="jIlbrmwX49A" bind:this={ytCont}>
+					<div class="play-button" />
+				</div>
 			</div>
 			<div class="flex-1 self-start">
 				<h3 class="text-xl text-primary font-bold mb-4">Más videos:</h3>
 				<ul class="list-disc text-primarylight underline pl-4 text-lg font-medium">
-					<li>
-						<a href="https://youtu.be/3ebdT56LOT0" target="_blank" rel="noreferrer">Instalación</a>
-					</li>
-					<li>
-						<a href="https://youtu.be/EoCDqeZXrIo" target="_blank" rel="noreferrer"
-							>Clases o materias</a>
-					</li>
-					<li>
-						<a href="https://youtu.be/4QgcE2l8dtg" target="_blank" rel="noreferrer"
-							>Periodos (Trimestres)</a>
-					</li>
-					<li>
-						<a href="https://youtu.be/oUQ0prTDRkQ" target="_blank" rel="noreferrer"
-							>Administración de estudiantes</a>
-					</li>
-					<li>
-						<a href="https://youtu.be/aHvqUywurGo" target="_blank" rel="noreferrer">Asistencias</a>
-					</li>
-					<li>
-						<a href="https://youtu.be/YqL-oM8f53k" target="_blank" rel="noreferrer"
-							>Actividates (tareas, exámenes, etc.)</a>
-					</li>
+					{#each videos as video}
+						<li>
+							<a href={video.url} target="_blank" rel="noreferrer">{video.label}</a>
+						</li>
+					{/each}
 				</ul>
 			</div>
 		</div>
